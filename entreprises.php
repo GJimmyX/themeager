079b27ec0cf22c54d3931031ac9af094fd7ebca2
GJimmyX <gilbertg.jimmy@gmail.com>
2022-05-13 16:30:12 +0200

    Ajout dossier images

--- entreprises.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /* Template Name: Entreprises */
-?>
-
-<?php get_header(); ?>
-
-<?php get_footer(); ?>
--- mentions-legales.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /* Template Name: Mentions légales */
-?>
-
-<?php get_header(); ?>
-
-<?php get_footer(); ?>
